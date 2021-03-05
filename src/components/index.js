bacb64b55b5bb2cdf61b818ce889fea3c74a5d1a
RobOrtiz <mrrobortiz@gmail.com>
2021-03-04 17:09:58 -0800

    Added Footer component

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css'
+
+class Footer extends Component {
+    render() {
+    return (
+        <div>
+            <footer id="sticky-footer" className="py-4 bg-dark text-white-50">
+                <div className="container text-center">
+                <small>Copyright &copy; Robert Ortiz Portfolio</small>
+                </div>
+            </footer>
+         </div>
+        );
+    }
+}
+
+export default Footer;
