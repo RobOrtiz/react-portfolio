7a31acc2925790dc89ad28369d2ef4ca7ed011d2
RobOrtiz <mrrobortiz@gmail.com>
2021-03-04 22:19:25 -0800

    Updates

---
+++ precache-manifest.34821ce7a7dc5bda9ab3b942ba85e88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d70e2d86f6d7a33145",
+    "url": "/react-portfolio/static/css/main.7a96884a.chunk.css"
+  },
+  {
+    "revision": "b5d70e2d86f6d7a33145",
+    "url": "/react-portfolio/static/js/main.d9cde75b.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "0ae42c8183f57be4c11a",
+    "url": "/react-portfolio/static/css/2.e34aa383.chunk.css"
+  },
+  {
+    "revision": "0ae42c8183f57be4c11a",
+    "url": "/react-portfolio/static/js/2.513874ea.chunk.js"
+  },
+  {
+    "revision": "502e0a1b9c5e37345af4cb20461def4f",
+    "url": "/react-portfolio/index.html"
+  }
+];
